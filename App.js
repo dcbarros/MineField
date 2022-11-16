f8b70720685f2468e698e0e6f3ea26638f8d5d69
Davison Cavalcante <dcbarros1@gmail.com>
2022-11-16 11:41:58 -0300

    Criação do tabuleiro do campo minado

--- App.js
+++ App.js
@@ -1,33 +1,59 @@
 import React, { Component } from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
 import params from './src/parans';
-import Field from './src/componets/Field'
+import { createMinedBoard } from './src/functions';
+
+import MinedField from './src/componets/MinedField';
+
+
 
 export default class App extends Component {
-    render() {
-        return (
-          <SafeAreaView style = {styles.container}>
-            <Text> Iniciando o Campo Minado</Text>
-            <Text> Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
-            <Field />
-            <Field opened/>
-            <Field opened nearMines = {1}/>
-            <Field mined/>
-            <Field mined opened/>
-            <Field mined opened exploded/>
-            <Field flagged/>
-            <Field flagged opened/>
-          </SafeAreaView>
-        )
+  
+  constructor(props){
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const rows = params.getRowsAmount()
+    const cols = params.getColumnsAmount()
+    return Math.ceil(cols*rows*params.difficultLevel)
+  }
+
+  createState = () => {
+    const rows = params.getRowsAmount()
+    const cols = params.getColumnsAmount()
+
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount),
     }
+
+  }
+
+  render() {
+    return (
+      <SafeAreaView style = {styles.container}>
+        <Text> Iniciando o Campo Minado</Text>
+        <Text> Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+        <View style = {styles.board}>
+          <MinedField board={this.state.board} />
+        </View>
+      </SafeAreaView>
+    )
+  }
 
 }
 
 const styles = StyleSheet.create({
+
   container:{
     flex:1,
-    justifyContent: 'center',
-    alignItems:'center',
+    justifyContent: 'flex-end',
   },
+
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA',
+  }
 })
